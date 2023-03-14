962901d1321a4665b6132d29aed6b930da53a596
윤태영 <dlem7556@naver.com>
2023-03-14 16:40:09 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -4,26 +4,17 @@ import styled, { ThemeProvider } from 'styled-components/native';
 import { theme } from './theme';
 import Input from './components/Input';
 import Task from './components/Task';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-const Container = styled.SafeAreaView`
+import { AppLoading } from 'expo';
 
-  flex: 1;
-  background-color: ${({ theme }) => theme.background};
-  align-items: center;
-  justify-content: flex-start;
-`;
-const Title = styled.Text`
-  font-size: 40px;
-  font-weight: 600;
-  color: ${({ theme }) => theme.text1}; // 변경된 부분
-  align-self: flex-start;
-  margin: 20px;
-`;
-const List = styled.ScrollView`
-  flex: 1;
-  width: ${({ width }) => width - 40}px;
-`;
+
+const Container = styled.SafeAreaView `flex: 1; background-color: ${({ theme }) => theme.background}; align-items: center; justify-content: flex-start;`;
+
+const Title = styled.Text` font-size: 40px; font-weight: 600; color: ${({ theme }) => theme.text1}; align-self: flex-start; margin: 20px;`;
+
+const List = styled.ScrollView`flex: 1; width: ${({ width }) => width - 40}px;`;
+
 const Footer = styled.View`
   border-top-width: 1px;
   border-color: ${({ theme }) => theme.text};
@@ -31,64 +22,76 @@ const Footer = styled.View`
   padding: 10px 0;
   flex-direction: row;
   justify-content: center;
+  align-items: stretch;
 `;
 
 const DeleteAllButton = styled.Button.attrs({
   title: '완료 항목 일괄 삭제',
 })`
   color: ${({ theme }) => theme.done};
+  flex: 1;
+  margin: 0 10px;
 `;
+
 export default function App() {
-  //const width = Dimensions.get('window').width는 현재 창의 너비를 width 변수에 할당하는 것
-  const width = Dimensions.get('window').width;
+const width = Dimensions.get('window').width;
 
-  const [isReady, setIsReady] = useState(false);
-  const [newTask, setNewTask] = useState('');
-  const [tasks, setTasks] = useState({});
- 
-  //tasks를 AsyncStorage에 저장.
-  const _saveTasks = async tasks => {
-    try {
-      await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
-      setTasks(tasks);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-  // AsyncStorage에서 tasks를 가져와 state에 저장.
-  const _loadTasks = async () => {
-    const loadedTasks = await AsyncStorage.getItem('tasks');
-    setTasks(JSON.parse(loadedTasks || '{}'));
-  };
-  //추가
-  const _addTask = () => {
-    const ID = Date.now().toString();
-    const newTaskObject = {
-      [ID]: { id: ID, text: newTask, completed: false },
-    };
-    setNewTask('');
-    _saveTasks({ ...tasks, ...newTaskObject });
-  };
-  //삭제
-  const _deleteTask = id => {
-    const currentTasks = Object.assign({}, tasks);
-    delete currentTasks[id];
-    _saveTasks(currentTasks);
-  };
+const [isReady, setIsReady] = useState(false);
+const [newTask, setNewTask] = useState('');
+const [tasks, setTasks] = useState({});
 
-  //완료 여부
-  const _toggleTask = id => {
-    const currentTasks = Object.assign({}, tasks);
-    currentTasks[id]['completed'] = !currentTasks[id]['completed'];
-    _saveTasks(currentTasks);
-  };
+// Splash Screen 렌더링 중이면 AppLoading 화면 표시
+if (!isReady) {
+SplashScreen.preventAutoHideAsync(); // Splash Screen 자동 종료 방지
+}
 
-  //update 함수
-  const _updateTask = item => {
-    const currentTasks = Object.assign({}, tasks);
-    currentTasks[item.id] = item;
-    _saveTasks(currentTasks);
-  };
+// AppLoading 끝난 후 호출되는 함수
+const _loadTasks = async () => {
+const loadedTasks = await AsyncStorage.getItem('tasks');
+setTasks(JSON.parse(loadedTasks || '{}'));
+setIsReady(true); // 끝나면 Splash Screen 종료
+};
+
+// tasks를 AsyncStorage에 저장.
+const _saveTasks = async (tasks) => {
+try {
+await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
+setTasks(tasks);
+} catch (e) {
+console.error(e);
+}
+};
+
+// 항목 추가
+const _addTask = () => {
+const ID = Date.now().toString();
+const newTaskObject = {
+[ID]: { id: ID, text: newTask, completed: false },
+};
+setNewTask('');
+_saveTasks({ ...tasks, ...newTaskObject });
+};
+
+// 항목 삭제
+const _deleteTask = (id) => {
+const currentTasks = Object.assign({}, tasks);
+delete currentTasks[id];
+_saveTasks(currentTasks);
+};
+
+// 완료 여부 변경
+const _toggleTask = (id) => {
+const currentTasks = Object.assign({}, tasks);
+currentTasks[id]['completed'] = !currentTasks[id]['completed'];
+_saveTasks(currentTasks);
+};
+
+// 항목 수정
+const _updateTask = (item) => {
+const currentTasks = Object.assign({}, tasks);
+currentTasks[item.id] = item;
+_saveTasks(currentTasks);
+};
  // Input Componet text가 Change되었을 때 실행되는 함수.
   const _handleTextChange = text => {
     setNewTask(text);
